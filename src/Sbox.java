01e34911aa5ee65b6c203d7a172bb095e991405a
PhoB <psh0036@naver.com>
2025-05-04 21:01:57 +0900

    make sbox method

---
+++ src/Sbox.java
@@ -0,0 +1,78 @@
+
+public class Sbox {
+    private static final int SBOX_Y = 0x63;
+    private static final int[][] MATRIX_X = {
+            {1, 0, 0, 0, 1, 1, 1, 1},
+            {1, 1, 0, 0, 0, 1, 1, 1},
+            {1, 1, 1, 0, 0, 0, 1, 1},
+            {1, 1, 1, 1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 1, 1, 1, 1, 1}
+    };
+    private static final int[][] MATRIX_X_INV = {
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 0, 0, 1, 0},
+            {0, 1, 0, 0, 1, 0, 0, 1},
+            {1, 0, 1, 0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0, 0, 1, 0},
+            {0, 0, 1, 0, 1, 0, 0, 1},
+            {1, 0, 0, 1, 0, 1, 0, 0},
+            {0, 1, 0, 0, 1, 0, 1, 0}
+    };
+    private final GfCalc gf;
+    public Sbox(){
+        this.gf = new GfCalc();
+    }
+
+    // S-Box 생성
+    public int subBytes(int value){
+        int inv_value = gf.gfInverse(value);
+        return matrixMultiply(byteToMatrix(inv_value)) ^ SBOX_Y;
+    }
+
+    public int invSubBytes(int value){
+        int[] vec = byteToMatrix(value ^ SBOX_Y);
+        int preimage = matrixMultiplyInv(vec);
+        return gf.gfInverse(preimage); // Inv(S(x)) = Inv(A*x ⊕ c)
+    }
+    private int[] byteToMatrix(int value){
+        int[] matrix = new int[8];
+        for(int i = 0; i<8; i++){
+            matrix[i] = (value >> i) & 0x01;
+        }
+        return matrix;
+    }
+    private int matrixMultiply(int[] matrix){
+        int result = 0;
+        for(int i = 0; i<8; i++){
+            int bit = 0;
+            for(int k = 0; k<8; k++){
+                if(MATRIX_X[i][k] == 1 && matrix[k] == 1){
+                    bit ^= 1;
+                }
+            }
+            if(bit == 1){
+                result |= (1 << i);
+            }
+        }
+        return result;
+    }
+
+    private int matrixMultiplyInv(int[] matrix){
+        int result = 0;
+        for(int i = 0; i<8; i++){
+            int bit = 0;
+            for(int k = 0; k<8; k++){
+                if(MATRIX_X_INV[i][k] == 1 && matrix[k] == 1){
+                    bit ^= 1;
+                }
+            }
+            if(bit == 1){
+                result |= (1 << i);
+            }
+        }
+        return result;
+    }
+}
