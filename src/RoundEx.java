99b7e7b1c9f7b37421e1c592fad6b9f0c71ed734
PhoB <psh0036@naver.com>
2025-05-06 22:58:39 +0900

    add each inverse function

--- src/RoundEx.java
+++ src/RoundEx.java
@@ -8,18 +8,22 @@ public class RoundEx {
             {1,1,2,3},
             {3,1,1,2}
     };
+    private static final int[][] INV_COLUMNS_MATRIX = {
+            {0x0E, 0x0B, 0x0D, 0x09},
+            {0x09, 0x0E, 0x0B, 0x0D},
+            {0x0D, 0x09, 0x0E, 0x0B},
+            {0x0B, 0x0D, 0x09, 0x0E}
+    };
     private final GfCalc gf;
     private final Sbox sbox;
-    int round = 0;
 
-    public RoundEx(int[][] inputState, byte[][] key, int round) {
+    public RoundEx(int[][] inputState, byte[][] key) {
         this.state = new int[4][4];
         for(int i = 0; i<4; i++){
             System.arraycopy(inputState[i],0, this.state[i],0, 4);
         }
         this.key = key;
         this.gf = new GfCalc();
-        this.round = round;
         this.sbox = new Sbox();
     }
 
@@ -37,6 +41,11 @@ public class RoundEx {
         state[2] = leftRotate(state[2], 2);
         state[3] = leftRotate(state[3], 3);
     }
+    private void invShiftRows(){
+        state[1] = rightRotate(state[1], 1);
+        state[2] = rightRotate(state[2], 2);
+        state[3] = rightRotate(state[3], 3);
+    }
 
     // 헬퍼 함수 추가
     private int[] leftRotate(int[] row, int n) {
@@ -46,9 +55,12 @@ public class RoundEx {
         }
         return result;
     }
-
-    private void invShiftRows(){
-
+    private int[] rightRotate(int[] row, int n){
+        int[] result = new int[4];
+        for(int i = 0; i<4; i++){
+            result[i] = row[(i - n + 4) % 4];
+        }
+        return result;
     }
 
     private void mixColumns(){
@@ -63,6 +75,18 @@ public class RoundEx {
         }
         state = temp;
     }
+    private void invMixColumns(){
+        int[][] temp = new int[4][4];
+        for(int col = 0; col < 4; col++){
+            for(int row = 0; row < 4; row++){
+                temp[row][col] = 0;
+                for(int i = 0; i<4; i++){
+                    temp[row][col] ^= gf.multiply(INV_COLUMNS_MATRIX[row][i], state[i][col]);
+                }
+            }
+        }
+        state=temp;
+    }
 
     private void subBytesInRound() {
         for (int row = 0; row < 4; row++) {
@@ -71,9 +95,14 @@ public class RoundEx {
             }
         }
     }
-    private void invMixColumns(){
-
+    private void invSubBytesInRound() {
+        for (int row = 0; row < 4; row++) {
+            for(int col = 0; col < 4; col++){
+                state[row][col] = sbox.invSubBytes(state[row][col] & 0xFF);
+            }
+        }
     }
+
 
     public void firstRound(){
         addRoundKey();
@@ -93,5 +122,16 @@ public class RoundEx {
     }
     public int[][] getState() {
         return state;
+    }
+    public void invMiddleRound(){
+        invShiftRows();
+        invSubBytesInRound();
+        addRoundKey();
+        invMixColumns();
+    }
+    public void invLastRound(){
+        invShiftRows();
+        invSubBytesInRound();
+        addRoundKey();
     }
 }
