736c7863ed6dca7f4c348a0f2e5941e0b8cd1521
PhoB <psh0036@naver.com>
2025-05-06 18:37:18 +0900

    div test method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 public class Main {
     public static byte[] roundKeys;
-    public static void main(String[] args) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public static void main(String[] args) {
         // GF(2^8) 곱셈 테스트
 
-        String Key = "";
+        String Key = "2b7e151628aed2a6abf7158809cf4f3c";
+        String PlainText = "6bc1bee22e409f96e93d7e117393172a";
         KeySchedule ks = new KeySchedule(Key);
         roundKeys = ks.genKey(Key);
         int[][] state = ks.convertState(PlainText);
@@ -28,8 +22,6 @@ public class Main {
             }
             state = roundEx.getState();
         }
-
-        correctTest();
     }
     private static void printCiphertext(int[][] state) {
         System.out.print("암호문: ");
@@ -41,43 +33,5 @@ public class Main {
         System.out.println();
     }
 
-    private static void correctTest() throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        // 입력값
-        String keyHex = "2b7e151628aed2a6abf7158809cf4f3c";
-        String plainHex = "6bc1bee22e409f96e93d7e117393172a";
 
-        // HEX 문자열 → 바이트 배열 변환
-        byte[] keyBytes = hexStringToByteArray(keyHex);
-        byte[] plainBytes = hexStringToByteArray(plainHex);
-
-        // 키 객체 생성
-        SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-
-        // Cipher 인스턴스 생성 (AES/ECB/NoPadding 사용)
-        Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding"); // PKCS5Padding 아님!
-
-        // 암호화 수행
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encrypted = cipher.doFinal(plainBytes);
-
-        // 결과 출력 (Hex로 출력)
-        System.out.println("암호문: " + bytesToHex(encrypted));
-
-    }
-    public static byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i+1), 16));
-        }
-        return data;
-    }
-    public static String bytesToHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            sb.append(String.format("%02X", b));
-        }
-        return sb.toString();
-    }
 }
